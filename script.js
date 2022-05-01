355a33bc3848cf0d9d99b0613bbb048102daae8b
SULDUZ <shaiksulduz238@gmail.com>
2022-05-01 14:40:23 +0530

    added weather  icon

--- script.js
+++ script.js
@@ -4,7 +4,16 @@ const input = document.querySelector(".city-input");
 const suloxxxxx = document.querySelector("#search-btn");
 const clear = document.querySelector("#clear-all");
 const data = document.querySelector(".data");
-const map = document.querySelector('#map')
+const map = document.querySelector("#map");
+const clock = document.querySelector(".clock");
+const time = document.querySelector(".time");
+const period = document.querySelector(".period");
+const tempInfo = document.querySelector(".weather-info");
+const weatherIcon = document.querySelector(".weather-icon");
+const weatherDesc = document.querySelector(".weather-desc");
+
+input.value = "tokyo";
+fetchWeatherData();
 
 function fetchWeatherData() {
     let cityName = input.value;
@@ -21,13 +30,22 @@ function fetchWeatherData() {
             "&appid=40505836c473231f7ee6e2bff31db894"
         )
         .then(function(response) {
-            // console.log(response.data);
-            weatherData["coordinates"] = response.data.coord;
-            weatherData["currTime"] = new Date(
-                (response.data.dt + response.data.timezone) * 1000
-            );
-            weatherData["cityName"] = response.data.name;
-            weatherData["timezone"] = response.data.timezone / (60 * 60);
+            console.log(response.data);
+            const data = response.data;
+            const weatherData = {
+                coordinates: data.coord,
+                currTime: new Date((data.dt + data.timezone) * 1000),
+                cityName: data.name,
+                timezone: data.timezone / (60 * 60),
+                iconURL: "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@4x.png",
+            };
+            //   weatherData["coordinates"] = response.data.coord;
+            // weatherData["currTime"] = new Date(
+            //     (response.data.dt + response.data.timezone) * 1000
+            // );
+            // weatherData["cityName"] = response.data.name;
+            // weatherData["timezone"] = response.data.timezone / (60 * 60);
+            // weatherData["icon"] = response.data.weather[0].icon;
             data.innerHTML = `
             City Name: ${weatherData.cityName}<br>
             Fetched on: ${weatherData.currTime}<br>
@@ -36,18 +54,29 @@ function fetchWeatherData() {
                 lon:${weatherData.coordinates.lon}<br>
             Timezone: ${weatherData.timezone}
             `;
-            map.style.display = "block"
-            map.src = "https://maps.google.com/maps?q=" + weatherData.cityName + "&output=embed"
 
+            //set main temp in degs
+            const tempInDeg = (response.data.main.temp - 273.15).toFixed(2);
+            tempInfo.innerHTML = tempInDeg + "°C";
+
+            //set weather icon from openweatherapi
+            weatherIcon.src = weatherData.iconURL;
+
+            //set weather description
+
+            map.style.display = "block";
+            map.src =
+                "https://maps.google.com/maps?q=" +
+                weatherData.cityName +
+                "&output=embed";
         })
         .catch(function(error) {
-            map.style.display = "none"
+            map.style.display = "none";
             alert("SORRY! please enter a valid city name🥴");
             if (error.cod == 404) console.log("Not a valid city name!😑🥺");
             else console.log(error);
         });
 }
-
 
 function checkEnter(e) {
     if (e.keyCode == 13) fetchWeatherData();
@@ -60,7 +89,11 @@ clear.addEventListener("click", function() {
     console.log("testing");
 });
 const weatherImg = {
-    sunny: './assets/sunny.png',
-    rainy: './assets/rainy.png',
-    cloudy: './assets/cloudy.png',
-}
+    sunny: "./assets/sunny.png",
+    rainy: "./assets/rainy.png",
+    cloudy: "./assets/cloudy.png",
+};
+
+// clock.addEventListener("click", function() {
+//     // clock.style.top = '-100px'
+// });
